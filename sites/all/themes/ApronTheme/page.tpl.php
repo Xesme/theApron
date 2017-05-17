a2e1086b08e187ed864ee1212370937179f581cf
Xia Amendolara <xiaesmeamen@me.com>
2017-05-17 11:35:00 -0700

    add layout styling, begin styling user menu, lunch commit

--- sites/all/themes/ApronTheme/page.tpl.php
+++ sites/all/themes/ApronTheme/page.tpl.php
@@ -2,14 +2,15 @@
 <div id='page-wrapper'>
   <div class="container">
     <?php print render($page['page_top']); ?>
+
+    <!-- dashboard/toolbar/navbar -->
     <header>
-      <!-- dashboard/toolbar -->
       <div class="dashboard">
         <?php print render($page['dashboard']); ?>
         <div id='logo'>
           <?php if ($logo): ?>
-            <a href="<?php print $base_path: ?>" title="<?php print t('Click here to return home')?>"></a>
-            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+            <a href="<?php print $base_path; ?>" title="<?php print t('Click here to return home');?>">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
           <?php endif; ?>
         </div>
         <nav>
@@ -18,24 +19,58 @@
           </div>
         </nav>
       </div>
+
       <!--  header -->
-      <div class="header">
+      <div class="header-wrap">
         <?php print render($page['header']); ?>
       </div>
     </header>
+
+    <!-- Main Wrap -->
+    <div class="main-wrap">
     <!-- sidebar -->
-    <div class="sidebar">
+    <div class="sidebar-wrap">
+      <!-- social feed icons -->
+        <div class="social-wrap">
+          <?php print render($page['social']); ?>
+        </div>
+
       <?php print render($page['sidebar']); ?>
     </div>
+
     <!-- content -->
-    <div class="content">
-      <?php print render($page['content']); ?>
+      <!-- breadcrumb trail  -->
+      <?php if ($breadcrumb): ?>
+        <?php print $breadcrumb; ?>
+      <?php endif; ?>
+
+      <!--  title  -->
+      <!-- <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?> -->
+
+    <!-- tabs -->
+      <?php if ($tabs): ?>
+        <?php print render($tabs); ?>
+      <?php endif; ?>
+
+    <!-- error messages -->
+      <?php print $messages; ?>
+
+    <!-- content of the page -->
+      <div class="content-wrap">
+        <?php print render($page['content']); ?>
+      </div>
     </div>
-    <!-- footer -->
-    <div class="footer">
-      <?php print render($page['footer']); ?>
-    </div>
-    <?php print render($page['page_bottom']); ?>
-  </div>  <!-- end of container -->
+
+  <!-- footer -->
+  <div class="footer-wrap">
+    <?php print render($page['footer']); ?>
+  </div>
+
+  <?php print render($page['page_bottom']); ?>
+</div>  <!-- end of container -->
 </div>
 <!-- end of document  -->
