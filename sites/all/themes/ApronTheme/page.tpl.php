a909a3aecbda66961717d74f17ebfb39df9e4ccc
Xia Amendolara <xiaesmeamen@me.com>
2017-05-17 09:15:41 -0700

    add page.tpl.php page for custom theme

---
+++ sites/all/themes/ApronTheme/page.tpl.php
@@ -0,0 +1,30 @@
+<!-- begin document -->
+<div id='page-wrapper'>
+  <div class="container">
+    <header>
+      <!-- dashboard/toolbar -->
+      <div class="dashboard">
+        <div id='logo'>
+        </div>
+        <nav> </nav>
+      </div>
+      <!--  header -->
+      <div class="header">
+
+      </div>
+    </header>
+    <!-- sidebar -->
+    <div class="sidebar">
+
+    </div>
+    <!-- content -->
+    <div class="content">
+
+    </div>
+    <!-- footer -->
+    <div class="footer">
+
+    </div>
+  </div>  <!-- end of container -->
+</div>
+<!-- end of document  -->
