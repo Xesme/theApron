3ff33462b969da394968de0816984e7f396f65f0
Xia Amendolara <xiaesmeamen@me.com>
2017-05-18 13:12:36 -0700

    add greeting module

--- sites/all/themes/ApronTheme/js/scripts.js
+++ sites/all/themes/ApronTheme/js/scripts.js
@@ -1,12 +1,12 @@
 jQuery(document).ready(function($){
   $('#edit-preview').click(function () {
     $(this).css('background-color', 'Tomato');
-    $(this).css('box-shadow', '0 0 5px white');
+    $(this).css('box-shadow', '0px 0px 5px white');
   });
 
   $('#edit-submit').click(function () {
     $(this).css('background-color', 'MediumSeaGreen');
-    $(this).css('box-shadow', '0 0 5px white');
+    $(this).css('box-shadow', '0px 0px 5px white');
 
   });
 });
